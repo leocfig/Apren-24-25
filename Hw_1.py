47b10d2d810c06c7317859b0bfa56b19e8416b08
Raquel Rodrigues <raquel.c.rodrigues@tecnico.ulisboa.pt>
2024-09-22 10:50:11 +0100

    added preprocessing of data and anova test

--- Hw_1.py
+++ Hw_1.py
@@ -1,4 +1,30 @@
-from scipy.io import arff
+from scipy.io.arff import loadarff
 from sklearn.feature_selection import f_classif
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Loading the dataset
+data = loadarff("diabetes.arff")
+df = pd.DataFrame(data[0])
+
+# Check the first few rows of the dataframe
+#print(df.head())
+
+# Decoding the byte string class labels
+df['Outcome'] = df['Outcome'].str.decode('utf-8')
+
+# Separate the features from the target
+X = df.drop(columns=['Outcome'])        # Features (8 biological features)
+y = df['Outcome']                       # Target (normal/diabetes)
+
+#print(y.unique())
+
+# Using ANOVA to determine the input variables with the worst 
+# and best discriminative power
+f_values = f_classif(X, y)[0]
+best_power = X.columns[f_values.argmax()]
+worst_power = X.columns[f_values.argmin()]
+
+print(f"The input variable with the best discriminative power is {best_power}")
+print(f"The input variable with the worst discriminative power is {worst_power}")
